e40731a307f64f359e4bfa72630e39edf5ceae7f
Arcanae <justin.courdesse@gmail.com>
2017-10-02 11:26:37 +0200

    first tests

---
+++ script/premier.spec.js
@@ -0,0 +1,10 @@
+it('should be true', () => {
+    expect(true).toBe(true);
+});
+describe('function addition', () => {
+
+    it('should be 4', () => {
+        expect(addition(2, 2)).toBe(4);
+    });
+
+});
