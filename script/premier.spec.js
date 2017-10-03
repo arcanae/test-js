434f19adf17bf0fceda154647d988c8bd32550ee
Arcanae <justin.courdesse@gmail.com>
2017-10-03 10:17:37 +0200

    ex compteurs

--- script/premier.spec.js
+++ script/premier.spec.js
@@ -1,10 +1,21 @@
 it('should be true', () => {
     expect(true).toBe(true);
 });
-describe('function addition', () => {
 
-    it('should be 4', () => {
-        expect(addition(2, 2)).toBe(4);
+describe('function additionner', () => {
+    it('should add 2 to 2', () => {
+        let resultat = additionner(2, 2);
+        expect(resultat).toBe(4);
     });
 
+    it('should add with negative numbers', () => {
+        expect(additionner(-2, -2)).toBe(-4);
+    });
+
+
+    it('should throw error with string params', () => {
+
+        expect(additionner.bind(null, 'bloup', 2))
+            .toThrowError();
+    });
 });
